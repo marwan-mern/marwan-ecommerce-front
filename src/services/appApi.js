d56910b37732919c0712f65a8c23f54b2fbd44fb
marwan-mern <118802926+marwan-mern@users.noreply.github.com>
2023-05-02 22:04:46 +0300

    Update appApi.js

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const appApi = createApi({
     reducerPath: "appApi",
     // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080" }),
-    baseQuery: fetchBaseQuery({ baseUrl: "https://marwan-ecommerce-mern-app.herokuapp.com/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://marwan-ecommerce-app.onrender.com/" }),
     endpoints: (builder) => ({
         signup: builder.mutation({
             query: (user) => ({
